adb1db7799c07cbe8259129bb27b38a7c494a3fb
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-20 11:26:09 -0300

    refactor: remove sleep function

--- src/infra/gateways/payment.gateway.ts
+++ src/infra/gateways/payment.gateway.ts
@@ -1,11 +1,8 @@
 import { PaymentGatewayInterface } from '@/application/contracts/payment-gateway.interface'
 import { ProcessPaymentInput } from '@/application/usecases/process-payment.usecase'
-import { sleep } from '@/application/shared/helpers/sleep'
 
 export class PaymentGateway implements PaymentGatewayInterface {
   async process (input: ProcessPaymentInput): Promise<PaymentGatewayInterface.OutPut> {
-    await sleep(30000)
-
     const { card } = input
 
     const lastCardNumber: string = card.number.slice(-1)
